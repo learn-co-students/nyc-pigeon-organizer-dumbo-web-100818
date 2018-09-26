78b6f16b1da217058f7b8750e7739fe6a323152d
github username <github email address>
2018-09-26 18:59:52 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hashy = {}
+
+  data.each do |sym, stat|
+    stat.each do |k, v|
+      v.each do |x|
+      if !hashy.has_key?(x)
+        hashy[x] = {}
+      end
+
+      if !hashy[x].has_key?(sym)
+        hashy[x][sym] = []
+      end
+      if !hashy[x][sym].include? (k)
+        hashy[x][sym] << k.to_s
+
+    end
+    end
+  end
+  end
+hashy
 end
