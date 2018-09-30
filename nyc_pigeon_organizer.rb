395d1c38d5d822aab10ab5546c7f97cb7cd58b18
Carson Rey <c.rey494@gmail.com>
2018-09-30 00:27:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |csl, info_hash|
+    info_hash.each do |category, names_array|
+      names_array.each do |name|
+        if pigeon_list[name] == nil
+           pigeon_list[name] = {csl => []}
+           pigeon_list[name][csl] << category.to_s
+        else
+          if pigeon_list[name].include?(csl)
+            pigeon_list[name][csl] << category.to_s
+           
+          else
+          pigeon_list[name][csl] = []     
+          pigeon_list[name][csl] << category.to_s
+          end
+        end
+      end
+    end
+  end
+  pigeon_list
 end
+
+#how do i make this dry code?
+
+# iterate through the 'data' hash, find the pidgeon name and set that name as a key pointing to a hash, containing the 'csl' attriubte as a key pointing to an array. If the reference to a list of pidgeons includes a name, push that reference into the array. 
