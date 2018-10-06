0f33504210c22a87d7ff7e006401bd242a35549c
hmalik88 <hbmalik88@gmail.com>
2018-10-06 00:03:46 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ new_hash = {}
+ data.each do |stats, sub_stats|
+ 	sub_stats.each do |individual_stat, pigeons_who_match|
+ 		pigeons_who_match.each do |pigeon|
+ 			new_hash[pigeon] = {} if !new_hash[pigeon]
+ 			new_hash[pigeon][stats] = [] if !new_hash[pigeon][stats]
+ 			new_hash[pigeon][stats] << individual_stat.to_s if data[stats][individual_stat].include?(pigeon) 
+ 		end
+ 	end
+ end
+new_hash
 end
